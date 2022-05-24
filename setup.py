5ff06e37e1d5e3e2569ee36dfaab2e04a6bc0ff1
lrbenitez <luis.rodriguez@uclm.es>
2022-05-24 12:57:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = "0.4.1"
+
+setup(name='TS_Evolutionary_Prototyping',
+      version = __version__,
+      description='A library to find the prototype in large sets of series',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Pablo Leon-Alcaide',
+      author_email='Luis.Rodriguez@uclm.es',
+      url="https://github.com/lrbenitez/TS_Evolutionary_Prototyping",
+      py_modules = ['TS_Evolutionary_Prototyping'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Beta',
+
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering',
+      ],
+      keywords='lib Time Series prototyping',
+
+      packages=find_packages(exclude=["docs"]),
+
+      install_requires = ['numpy','scipy',
+                          'sklearn',
+                          'numba',
+                          'pandas',                   
+                          'matplotlib'
+                          'deap'],
+
+      test_require = ['colorednoise'],
+      
+      extras_require = {
+          'doc': ["sphinx", "numpydoc"]
+          }
+)
